fc1df9567030b9cf98375b33ca2007a8955112df
VitorLelis <vitorll9090@gmail.com>
2024-03-07 19:31:14 +0000

    feat: cloudflare wrap

--- setup.sh
+++ setup.sh
@@ -24,3 +24,10 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 sudo apt -f install
+
+#Cloudflare Wrap
+curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+
+sudo apt-get update && sudo apt-get install cloudflare-warp
